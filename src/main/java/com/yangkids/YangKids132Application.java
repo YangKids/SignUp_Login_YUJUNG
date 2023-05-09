b9a464b14b04924c1118c6a0079b831d0e8b6ff4
YUJUNG <heoyujung0118@gmail.com>
2023-05-09 22:32:28 +0900

    test1 branch commit test

--- src/main/java/com/yangkids/YangKids132Application.java
+++ src/main/java/com/yangkids/YangKids132Application.java
@@ -3,6 +3,8 @@ package com.yangkids;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// 수정수정
 @SpringBootApplication
 public class YangKids132Application {
 
